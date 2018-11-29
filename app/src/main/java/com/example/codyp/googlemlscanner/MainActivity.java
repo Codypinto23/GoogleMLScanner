3892962ace38372c7320b186ea86cc83a128eb72
Cody Pinto <codypinto23@gmail.com>
2018-11-29 07:57:59 -0700

    Initial Commit of ML Scanner

---
+++ app/src/main/java/com/example/codyp/googlemlscanner/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.codyp.googlemlscanner;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.example.codyp.googlemlscanner.common.barcodescanning.BarcodeScanningProcessor;
+import com.example.codyp.googlemlscanner.common.barcodescanning.CameraSource;
+import com.example.codyp.googlemlscanner.common.barcodescanning.CameraSourcePreview;
+import com.example.codyp.googlemlscanner.common.barcodescanning.GraphicOverlay;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Demo app chooser which takes care of runtime permission requesting and allows you to pick from
+ * all available testing Activities.
+ */
+public final class MainActivity extends AppCompatActivity{
+    private static final String TAG = "ChooserActivity";
+    private static final int PERMISSION_REQUESTS = 1;
+
+
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
+
+    private TextView textView;
+    private SeekBar seekBarZoom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.text);
+        seekBarZoom=findViewById(R.id.seekbarZoom);
+
+        preview = (CameraSourcePreview) findViewById(R.id.firePreview);
+        if (preview == null) {
+            Log.d(TAG, "Preview is null");
+        }
+        graphicOverlay = (GraphicOverlay) findViewById(R.id.fireFaceOverlay);
+        if (graphicOverlay == null) {
+            Log.d(TAG, "graphicOverlay is null");
+        }
+
+        seekBarZoom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                cameraSource.updateZoom(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        if (allPermissionsGranted()) {
+            createCameraSource();
+            startCameraSource();
+        } else {
+            getRuntimePermissions();
+        }
+    }
+
+
+    private void createCameraSource( ) {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, graphicOverlay);
+        }
+        cameraSource.setMachineLearningFrameProcessor(new BarcodeScanningProcessor(textView));
+
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                preview.start(cameraSource, graphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        startCameraSource();
+    }
+
+    /** Stops the camera. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preview.stop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+
+
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission granted: " + permission);
+            return true;
+        }
+        Log.i(TAG, "Permission NOT granted: " + permission);
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/codyp/googlemlscanner/common/barcodescanning/VisionProcessorBase.java
@@ -0,0 +1,166 @@
+package com.example.codyp.googlemlscanner.common.barcodescanning;
+
+
+
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.support.annotation.GuardedBy;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+
+/**
+ * Abstract base class for ML Kit frame processors. Subclasses need to implement {@link
+ * #onSuccess(Bitmap, Object, FrameMetadata, GraphicOverlay)} to define what they want to with
+ * the detection results and {@link #detectInImage(FirebaseVisionImage)} to specify the detector
+ * object.
+ *
+ * @param <T> The type of the detected feature.
+ */
+public abstract class VisionProcessorBase<T> implements VisionImageProcessor {
+
+    // To keep the latest images and its metadata.
+    @GuardedBy("this")
+    private ByteBuffer latestImage;
+
+    @GuardedBy("this")
+    private FrameMetadata latestImageMetaData;
+
+    // To keep the images and metadata in process.
+    @GuardedBy("this")
+    private ByteBuffer processingImage;
+
+    @GuardedBy("this")
+
+    private FrameMetadata processingMetaData;
+
+    public VisionProcessorBase() {
+    }
+
+    @Override
+    public synchronized void process(
+            ByteBuffer data, final FrameMetadata frameMetadata, final GraphicOverlay
+            graphicOverlay) {
+        latestImage = data;
+        latestImageMetaData = frameMetadata;
+        if (processingImage == null && processingMetaData == null) {
+            processLatestImage(graphicOverlay);
+        }
+    }
+
+    // Bitmap version
+    @Override
+    public void process(Bitmap bitmap, final GraphicOverlay
+            graphicOverlay) {
+        detectInVisionImage(null /* bitmap */, FirebaseVisionImage.fromBitmap(bitmap), null,
+                graphicOverlay);
+    }
+
+    private synchronized void processLatestImage(final GraphicOverlay graphicOverlay) {
+        processingImage = latestImage;
+        processingMetaData = latestImageMetaData;
+        latestImage = null;
+        latestImageMetaData = null;
+        if (processingImage != null && processingMetaData != null) {
+            processImage(processingImage, processingMetaData, graphicOverlay);
+        }
+    }
+
+    private static final int RGB_MASK = 0x00FFFFFF;
+
+    public Bitmap invert(Bitmap original) {
+        // Create mutable Bitmap to invert, argument true makes it mutable
+        Bitmap inversion = original.copy(Bitmap.Config.ARGB_8888, true);
+
+        // Get info about Bitmap
+        int width = inversion.getWidth();
+        int height = inversion.getHeight();
+        int pixels = width * height;
+
+        // Get original pixels
+        int[] pixel = new int[pixels];
+        inversion.getPixels(pixel, 0, width, 0, 0, width, height);
+
+        // Modify pixels
+        for (int i = 0; i < pixels; i++)
+            pixel[i] ^= RGB_MASK;
+        inversion.setPixels(pixel, 0, width, 0, 0, width, height);
+
+        // Return inverted Bitmap
+        return inversion;
+    }
+
+    private void processImage(
+            ByteBuffer data, final FrameMetadata frameMetadata,
+            final GraphicOverlay graphicOverlay) {
+        FirebaseVisionImageMetadata metadata =
+                new FirebaseVisionImageMetadata.Builder()
+                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                        .setWidth(frameMetadata.getWidth())
+                        .setHeight(frameMetadata.getHeight())
+                        .setRotation(frameMetadata.getRotation())
+                        .build();
+
+        Bitmap bitmap = BitmapUtils.getBitmap(data, frameMetadata);
+        Bitmap invertedBitmap=(invert(bitmap));
+        detectInVisionImage(invertedBitmap,FirebaseVisionImage.fromBitmap(invertedBitmap),frameMetadata,graphicOverlay);
+ /*       detectInVisionImage(
+                bitmap, FirebaseVisionImage.fromByteBuffer(data, metadata), frameMetadata,
+                graphicOverlay);*/
+    }
+
+    private void detectInVisionImage(
+            final Bitmap originalCameraImage,
+            FirebaseVisionImage image,
+            final FrameMetadata metadata,
+            final GraphicOverlay graphicOverlay) {
+        detectInImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<T>() {
+                            @Override
+                            public void onSuccess(T results) {
+                                VisionProcessorBase.this.onSuccess(originalCameraImage, results,
+                                        metadata,
+                                        graphicOverlay);
+                                processLatestImage(graphicOverlay);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                VisionProcessorBase.this.onFailure(e);
+                            }
+                        });
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    protected abstract Task<T> detectInImage(FirebaseVisionImage image);
+
+    /**
+     * Callback that executes with a successful detection result.
+     *
+     * @param originalCameraImage hold the original image from camera, used to draw the background
+     *                            image.
+     */
+    protected abstract void onSuccess(
+            @Nullable Bitmap originalCameraImage,
+            @NonNull T results,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay);
+
+    protected abstract void onFailure(@NonNull Exception e);
+}
